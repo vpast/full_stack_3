4e8b17e0ebbe51facf2bae102d8709bc0b4dae9f
Sweet Heaven <dalverq@gmail.com>
2021-04-08 12:26:13 +0300

    task 2_3

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 
 const IndexPage = () => {
-  const [users, setUsers] = useState ([])
-  useEffect(() => {}, [])
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.items)
+      });
+  }, []);
   return (
     <div className="container users">
       {users.map((user) => {
-        return <div>{user.name}</div>
+        return <div className="user" key={user.id}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          </div>;
       })}
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
